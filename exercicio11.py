280676d1579f27eff1ad525ac448115abde76ce9
DANIELKENTARO <124891840+DANIELKENTARO@users.noreply.github.com>
2024-03-01 11:55:29 -0300

    Update exercicio11.py

--- exercicio11.py
+++ exercicio11.py
@@ -1,31 +1,33 @@
-a = float (input ("digite o primeiro lado do tri�ngulo"))
+a = float (input ("digite o primeiro lado do triagulo"))
 
-b = float (input ("digite o segundo lado do tri�ngulo"))
+b = float (input ("digite o segundo lado do triangulo"))
 
-c = float (input ("digite o terceiro lado do tri�ngulo"))
+c = float (input ("digite o terceiro lado do triangulo"))
 
-chequer = (a + b + c)/c
+chequer1 = b - c
+chequer2 = a - c
+chequer3 = a - b
+d = int (0)
+if (chequer1) < a < (b + c) and (chequer2) < b < (a + c) and (chequer3) < c < (a + b) or a == b == c:
+    d + 1
 
-d = ""
-
-if chequer > 0:
+if d == 1:
     
-	print ("� um tri�ngulo")
+	print ("e um triangulo")
 
-    	d = 1
 
-elif chequer < 0:
+elif d == 0:
     
-	print ("isso n�o � um tri�ngulo")
+	print ("isso nao e um triangulo")
 
 if a == b == c and d == 1:
 
-    print ("� um tri�ngulo equilatero")
+    print ("e um triangulo equilatero")
 
 elif a > b == c or b > a ==c or c > a == b and d == 1:
 
-    print ("� um tri�ngulo is�sceles")
+    print ("e um triangulo isosceles")
 
-else:
+elif a != b !=c:
 
-    print ("� um tri�ngulo escaleno")
+    print ("e um triangulo escaleno")
