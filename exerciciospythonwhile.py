ae3446806bc1f4c29888fb31b2d795d1f801df21
DANIELKENTARO <124891840+DANIELKENTARO@users.noreply.github.com>
2024-03-07 12:23:27 -0300

    Add files via upload

---
+++ exerciciospythonwhile.py
@@ -0,0 +1,20 @@
+#exercicio 1
+a = float (input ("digite um numero:"))
+if a < 0:
+    print ("numero menor que 0")
+elif a == 0:
+    print ("numero igual a 0")
+else:
+    print ("numero maior que 0")
+#exercicio 2
+b = int (input ("digite um de 1 a 10:"))
+if b == 2 or b == 4 or b == 6 or b ==8 or b == 10:
+    print ("numero par")
+else:
+    print("numero inpar")
+#exercicio 3
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+#exercicio 4
